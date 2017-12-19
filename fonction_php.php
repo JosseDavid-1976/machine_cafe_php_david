467e05002a25dc80755578fb114bdbeadbe90838
David Josse <david.josse@Digital-Grenoble.local>
2017-12-19 15:25:15 +0100

    Ca marche

--- fonction_php.php
+++ fonction_php.php
@@ -1,5 +1,7 @@
 <?php
-$nbrSucre = 5;
+
+$nbrSucre = $_POST["sucre"];
+$Boisson = $_POST["boisson"];
 function expresso($nbrSucre){
   echo "<li>Expresso : 1*Eau,1*Café,".$nbrSucre."*sucre(s)</li>";
 };
@@ -16,11 +18,25 @@ function preparerBoisson($Boisson,$nbrSucre){
       echo expresso($nbrSucre);
     }elseif ($Boisson === "cafeLong"){
       echo cafeLong($nbrSucre);
-    }elseif ($Boisson === "thé"){
+    }elseif ($Boisson === "the"){
       echo the($nbrSucre);
     }
     
   };
 ?>
 
-
+<!DOCTYPE html><!--creation de mon block html-->
+<html>
+  <head>
+  <title>preparerBoisson</title><!--titre de la page-->
+  <meta charset="utf-8">
+  
+  </head>
+  <body>
+    <h1>Fonction php</h1><!--nom de la machine -->
+  <p>
+    <?=preparerBoisson($Boisson,$nbrSucre);?>
+  </p>
+  
+</body>
+</html>
--- preparerBoisson.php
+++ preparerBoisson.php
@@ -14,9 +14,25 @@
   <body>
     <h1>Préparer Boisson</h1><!--nom de la machine -->
   <p>
-  <?= preparerBoisson("thé",$nbrSucre);?>
-  <?= preparerBoisson("expresso",$nbrSucre);?>
-  <?= preparerBoisson("cafeLong",$nbrSucre);?>
+ 
+  </p>
+  <p>
+        <form action="fonction_php.php" method="post">
+        <p>Choix Boisson</p>
+        <input type="radio" name="boisson" value="expresso" checked> Expresso<br>
+        <input type="radio" name="boisson" value="cafeLong"> Café Long<br>
+        <input type="radio" name="boisson" value="the"> Thé<br>
+        <p>Nombre de Sucre</p>
+        <input type="radio" name="sucre" value="0" checked> Sans<br>
+        <input type="radio" name="sucre" value="1"> 1<br>
+        <input type="radio" name="sucre" value="2"> 2<br>
+        <input type="radio" name="sucre" value="3" > 3<br>
+        <input type="radio" name="sucre" value="4"> 4<br>
+        <input type="radio" name="sucre" value="5"> 5<br>
+        <p></p>
+        <input type="submit" value="Preparer" >
+
+      </form> 
   </p>
 </body>
 </html>
